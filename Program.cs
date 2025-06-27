ed315b7d7710ff3baf6b4a324b1c6caa485d3dec
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2025-06-27 22:26:37 +0300

    ex 2 was done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
 ﻿
+var Numbers = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase)
+{
+    {"zero", 0 },{"one", 1 },
+    {"two", 2 },{"three", 3 },
+    {"four", 4 },{"five", 5 },
+    {"six", 6 },{"seven", 7 },
+    {"eight", 8 },{"nine", 9 } };
+while (true)
+{
+    Console.WriteLine("Enter word number - ");
+    string input = Console.ReadLine()?.Trim().ToLower();
+    if(Numbers.TryGetValue(input, out int num))
+    {
+        Console.WriteLine($"Number: {num}");
+    }
+    else
+    {
+        Console.WriteLine("Not correct. Error!");
+    }
+}
